2c2d7554a6493c55b0d11892d0e10fe1fb55fb1f
mandolyte <cecil.new@gmail.com>
2019-08-14 13:51:24 -0400

    first cut on data prep

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ If you want to suggest a change, please fork this repo and create a PR, or creat
 
 ## Building
 
+*Note there is now a data preparation step; see below*
+
 Install the sphinx engine
 
     sudo apt-get install python-sphinx
@@ -16,3 +18,31 @@ Then run the build script.
 > NOTE: this top level makefile is just a shorcut to building the html.
 
     make
+
+
+## Data Preparation
+
+In the data folder are two JSON files which must
+be refreshed manually at present. These two JSON
+files are inputs `bin/dataprep.py`.
+
+- `data/langnames.json`: this is downloaded from the 
+current tD production website as follows:
+    - Go to https://td.unfoldingword.org
+    - Click "Data Sources", then
+    - Click "Language Names JSON"
+    - Or use direct link: https://td.unfoldingword.org/langnames.json
+- `data/ln_gw.json`: this is created by the following SQL against the tD Postgres database:
+    ```sql
+    select u.code lc_code,g.code as gw_code 
+    from uw_language u 
+    left outer join uw_language g 
+    on u.gateway_language_id = g.id
+    ```
+    - using DBeaver, the result set is exported as JSON. Exporting notes:
+        - Toggle off the setting `Print table name`
+        - Use encoding UTF-8
+        - Toggle off setting to "Insert BOM"
+- `bin/dataprep.py`: this Python3 script will output:
+    - one folder per language (in `data/languages`)
+    - in each folder will be the original JSON from `langnames.json` with the addition of a key named "GW" and its value. This will be the Gateway Language (if any) for the language.
---
+++ bin/dataprep.py
@@ -0,0 +1,92 @@
+import json
+import os
+import re # for data subsetting
+
+basedirflag = True
+if not os.path.exists("./data") : basedirflag = False
+if not os.path.exists("./docs") : basedirflag = False
+if not os.path.exists("./bin") : basedirflag = False
+
+if not basedirflag :
+    print ("Note: this script should be run in the project root folder:")
+    print ("Thus: python3 bin/dataprep.py")
+    print ("These subdirs must exist: data, docs, and bin")
+    exit()
+
+# create the "data/languages"
+datalangpath = "./data/languages"
+if not os.path.exists(datalangpath):
+    try:
+        os.mkdir(datalangpath)
+    except OSError:
+        print ("Creation of directory %s failed" % datalangpath)
+        exit()
+    else:
+        print ("Successfully created directory %s " % datalangpath)
+
+#
+# open and parse the all languages JSON
+#
+with open("data/langnames.json", "r") as lnfile:
+    lndata = lnfile.read()
+    jlndata = json.loads(lndata)
+
+#
+# open and parse the gateway languages JSON
+#
+with open("data/ln_gw.json", "r") as gwfile:
+    gwdata = gwfile.read()
+    jgwdata = json.loads(gwdata)
+
+# load up the dictionary for lang and gw
+count=0
+gwdict = {}
+for gwj in jgwdata:
+    count = count+1
+    gwdict[gwj["lc_code"]] = gwj["gw_code"]
+
+print("\n\n\nTotal in ln/gw list: "+str(count))
+
+count=0
+for lnj in jlndata:
+    count=count+1
+    lc = lnj["lc"] # here is the language code
+    #print (lc)
+    ### DEBUG CODE ###
+    ### only actually create a few language dirs
+    tomatch = "^(ca|en|es|gaj|dnr|tpi)$"
+    ismatch = re.search(tomatch, lc)
+    if (not ismatch) : 
+        continue
+
+    # lookup the gateway language
+    try:
+        gwcode = gwdict[lc]
+    except KeyError:
+        print ("No gateway language for "+lc)
+        gwcode = "UNKNOWN"
+    else:
+        print ("Gateway language for "+lc+" is "+str(gwcode))
+
+    # add it to the language data
+    lnj["gwcode"] = gwcode
+
+    # create lang subdir if needed
+    path = datalangpath + "/" + lc
+    if not os.path.exists(path):
+        try:
+            os.mkdir(path)
+        except OSError:
+            print ("Creation of directory %s failed" % path)
+            exit()
+        else:
+            print ("Successfully created directory %s " % path)
+    
+    # now print the lang json to "lc".json
+    fpath = path + "/" + lc + ".json"
+    with open(fpath,"w") as fpath_handle:
+        json.dump(lnj,fpath_handle)
+
+
+
+print("\n\n\nTotal in all lang list: "+str(count))
