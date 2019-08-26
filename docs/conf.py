d294ea163881bb949dae97696d29ea1a9197e5ee
mandolyte <cecil.new@gmail.com>
2019-08-26 11:18:13 -0400

    tweaked conf.py setrecursionlimit parameter

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sys; sys.setrecursionlimit(9999)
+import sys; sys.setrecursionlimit(9998)
 
 # -- General configuration ------------------------------------------------
 
